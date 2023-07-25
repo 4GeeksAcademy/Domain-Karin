ea0506426c88e2f026eee44a78b29b240106cc44
KarinRG <131704279+KarinRG@users.noreply.github.com>
2023-07-25 01:33:07 +0000

    PROYECTO

--- src/app.js
+++ src/app.js
@@ -7,5 +7,20 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("Hello World!");
+
+  let pronoun = [" the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let domain = [".com", ".net", ".us", ".io"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let e = 0; e < adj.length; e++) {
+      for (let f = 0; f < noun.length; f++) {
+        for (let g = 0; g < domain.length; g++) {
+          console.log(pronoun[i] + adj[e] + noun[f] + domain[g]);
+        }
+      }
+    }
+  }
 };
